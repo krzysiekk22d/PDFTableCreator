7b1b7d3a289f8c50bff1974d2582acb3b15d85e5
krzysiekk22d <klas.club@gmail.com>
2023-11-25 17:35:36 +0100

    Initial Commit

---
+++ PDFTableCreator/PDFTableCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFTableCreatorApp.swift
+//  PDFTableCreator
+//
+//  Created by Krzysztof Czura on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PDFTableCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
